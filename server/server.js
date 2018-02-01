965dbea5db98ae0512066a19cd620bda6b9c9140
Mark Chen <markchen555@gmail.com>
2018-02-01 14:30:31 -0800

    Doc: modify server to have differnt build environment

--- server/server.js
+++ server/server.js
@@ -6,18 +6,36 @@ const db = require('../db/db');
 require('../db/model/dataModel')
 const route = require('../server/router/routes')
 
+const compression = require('compression');
+const helmet = require('helmet')
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = process.env.NODE_ENV === 'production';
+
+
 // const PORT = 3000;
 
 const app = express()
 app.set('port', (process.env.PORT || 3000))
-.use(parser.json())
-.use(parser.urlencoded({extended: true}))
-.use(morgan('dev'))
-.use('/api', route)
-.use(express.static(path.resolve(__dirname, '../client/static')))
-.get('/*', function (req, res) {
+
+if (isProd) {
+  app.use(compression());
+  app.use(helmet());
+}
+
+app.use(parser.json());
+app.use(parser.urlencoded({extended: true}));
+
+if (isDev) {
+  app.use(morgan('dev'));
+}
+
+app.use('/api', route)
+app.use(express.static(path.resolve(__dirname, '../client/static')))
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, '../client/static', 'index.html'));
 })
-.listen(app.get('port'), function(){
+
+app.listen(app.get('port'), function(){
   console.log(`Listening on port ${app.get('port')}`)
 })
