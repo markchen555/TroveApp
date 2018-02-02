f14abab6212584c54cac9827d30f51f3cd7d3cac
Mark Chen <markchen555@gmail.com>
2018-02-01 17:43:41 -0800

    Doc: modify webpack for deploy testing

--- webpack.config.js
+++ webpack.config.js
@@ -13,21 +13,6 @@ const webpackConfig = {
   resolve: {
     extensions: ['.js', '.jsx'],
   },
-  devtool: 'inline-source-map',
-  plugins: [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      minimize: true,
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    })
-  ],
 };
 
 webpackConfig.module.loaders.push({
